86ac8d489eb64957bece7b631402954a8977dc63
JeffersonToledo <abelktoledo@gmail.com>
2021-11-08 17:34:02 -0300

    array 01

--- index.php
+++ index.php
@@ -18,6 +18,8 @@
     $num = 0;
     $num1 = 0;
     $num3 = 0;
+    $num4 = 0;
+    $num5 = 0;
     
     $nome = [
       "Helena" => array(31,41,55,66,40,60),
@@ -77,9 +79,12 @@
     require_once "./conexao.php";
 
     $sql = "SELECT id,nome,number1,number2,number3,number4,number5,number6 FROM numeros";
+    $sql2 = "SELECT nome,number1,number2,number3,number4,number5,number6 FROM numeros";
 
     $conexao = $conexaoLogin;
     $resultado = $conexao->query($sql);
+    $resultado2 = mysqli_query($conexao,$sql2);
+
 
     $registros = [];
 
@@ -124,8 +129,6 @@
         <td><?= $registro['number4'] ?></td>
         <td><?= $registro['number5'] ?></td>
         <td><?= $registro['number6'] ?></td>
-          
-
 
       </tr>    
 
@@ -137,39 +140,162 @@
 
       <?php
 
-        while($row = $resultado->fetch_assoc()) {
+        $dadosGerais[] = $registros;
 
-          for($i = 0; $i < 2; $i++){
-            $dados[] = $row;
-            foreach($dados as $dd){
-              echo $dd;
-            }
-          }
-        }
+        // array_chunk($dadosGerais,6);
+
+        $teste = mysqli_num_fields($resultado2);
+
+        $num_linhas = mysqli_num_rows($resultado2);
+
+        echo $num_linhas . "<br><br>";
+
+        $dados = mysqli_fetch_row($resultado2);
+
+
+        $row2 = $resultado2 -> fetch_array(MYSQLI_ASSOC);
+        printf ("%s\n", $row2["nome"]);
+
+        echo "<br><br>";
+
+        // $row2 = $resultado2 -> fetch_array(MYSQLI_NUM);
+        // printf ("%s (%s,%s,%s,%s,%s,%s)\n", $row2[0], $row2[1],$row2[2],$row2[3],$row2[4],$row2[5],$row2[6]);
+
+        // print_r($registros[1]['nome']);
+
+        // foreach($dadosGerais as $ee){
+        //   for($i = 0; $i < $teste; $i++){
+        //     // echo $dados[$i];
+        //     foreach($array as $certo){         
+        //       if(intval($dados[$i]) === $certo){
+        //         $num3++;
+        //         if(3 >= $num3){
+        //           $name = $dados[0];
+                            
+        //         }
+        //       }
+        //     }
+        //   }
+        // }
+
+
         
 
-        
 
-        
-
-        foreach($registros as $val => $nn){
-          foreach($nn as $mm){
-          foreach($array as $certo){         
-            if(intval($mm) === $certo){
-              $num3++;
-              if(3 >= $num3){
-                $name = $nn['nome'];
-                          
+     
+          for($i = 0; $i < $teste; $i++){
+            // echo $dados[$i];
+            foreach($array as $certo){         
+              if(intval($dados[$i]) === $certo){
+                $num3++;
+                if(3 >= $num3){
+                  $name = $dados[0];
+                            
+                }
               }
             }
           }
-        }
-        }
+
+          for($a = 0; $a < $num_linhas; $a++)
+          {
+            
+          }
+
+
+        print_r($registros);
+        echo "<br><br>";
+        // for($j = 0; $j < $teste; $j++){
+        //   $array3[] =  $registros[$j]['nome'];
+          
+        // }
+
+        $array3 = $registros;
+
+        array_splice($array3,2);
+
+        // array_splice($array3,0,1);
+
+        print_r($array3);
+        echo "<br><br>";
+
+        // foreach($array3 as $der => $der2){
+        //   foreach($der2 as $der3){
+        //     echo $der3;
+        //   }
+        // }
+                
+     
+            foreach($array as $certo){
+              foreach($array3 as $der => $der2){
+                foreach($der2 as $der3){         
+                  if(intval($der3) === $certo){
+                    $num4++;
+                    if(3 >= $num4){
+                      $name2 = $der2['nome'];
+                                
+                    }
+                  }
+                }
+              }
+            }
+
+
+            $array4 = $registros;
+
+
+            array_splice($array4,0,2);
+
+            print_r($array4);
+
+            foreach($array as $certo){
+              foreach($array4 as $ter => $ter2){
+                foreach($ter2 as $ter3){         
+                  if(intval($ter3) === $certo){
+                    $num5++;
+                    if(3 >= $num5){
+                      $name3 = $ter2['nome'];
+                                
+                    }
+                  }
+                }
+              }
+            }
+        
+
+        // foreach($registros as $val => $nn){
+        //   foreach($nn as $mm){
+        //   foreach($array as $certo){         
+        //     if(intval($mm) === $certo){
+        //       $num3++;
+        //       if(3 >= $num3){
+        //         $name = $nn['nome'];
+                          
+        //       }
+        //     }
+        //   }
+        // }
+        // }
 
         
 
         echo "<br> Nome: " . $name;
         echo "<br> Números de acertos: " .  $num3;
+
+        echo "<br> Nome: " . $name2;
+        echo "<br> Números de acertos: " .  $num4;
+
+        echo "<br> Nome: " . $name3;
+        echo "<br> Números de acertos: " .  $num5;
+
+
+       echo "<br><br>";
+       
+
+        // foreach($registros as $far => $far2){
+        //   foreach($far2 as $far3){
+        //     echo array_search("Helena",$far3['nome']);
+        //   }
+        // }
 
       
       ?>
