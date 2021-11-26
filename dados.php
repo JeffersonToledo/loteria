92eed83b4c167796592f4f8b0ed2f63463fe6ef0
JeffersonToledo <abelktoledo@gmail.com>
2021-11-25 23:15:55 -0300

    logica02

--- dados.php
+++ dados.php
@@ -87,81 +87,200 @@
 
 
 
+    $ct = count($jogadores);
 
+    // print_r($jogadores);
         
   if($resultado2->num_rows > 0)
   {
 
-    $name01 = $jogadores[0];
-    $name02 = $jogadores[7];
-    $name03 = $jogadores[13];
+    // $name01 = $jogadores[0];
+    // $name02 = $jogadores[7];
+    // $name03 = $jogadores[13];
+    // $name04 = $jogadores[19];
+    // $name05 = $jogadores[25];
+    // $name06 = $jogadores[31];
+    // $name07 = $jogadores[37];
+    // $name08 = $jogadores[43];
+    // $name09 = $jogadores[49];
+    // $name10 = $jogadores[55];
+
+    $teste05 = [];
+
+    for($j = 0; $j <= $tamanho -1;$j++){
+      
+      $t = $j * 7;
+      $teste05[] = $jogadores[$t];
+    }
+
+    $tb = count($teste05);
+
+    $teste06 = [];
+
+    for($l = 1; $l<= $tamanho + 3;$l++){
+      
+      $z = $l;
+      $teste06[] = $jogadores[$z];
+    }
+
+    $string[] = implode(",", $teste06);
+
+    // print_r ($string);
+
+
+    // echo "<br><br>";
+
+    $array = array(33,44,55,66,77,88);
+
+    
+
+    // $namesVar = [];
+
+    // for($i = 0; $i <= $tb; $i++){
+    //   $name0[$i] = $teste05[$i];
+    // }
+
+    // echo $name01;
+
+   
 
     $num = 0;
     $num1 = 0;
     $num2 = 0;
-    
+    $num3 = 0;
+    $num4 = 0;
+    $num5 = 0;
+    $num6 = 0;
+    $num7 = 0;
+    $num8 = 0;
+    $num9 = 0;
+
 
     class Dados {
-      // Propriedades
       public $name;
+
+      public function __construct($_name)
+      {
+        $this->name = $_name;
+      }
+
+    }
+
+    class Numbers {
+
       public $numeros;
 
-      // Metodos
-      function set_name($name) {
-        $this->name = $name;
-      }
-      function get_name() {
-        return $this->name;
-      }
-
-      function set_numeros($numeros) {
-        $this->numeros = $numeros;
-      }
-      function get_numeros() {
-        return $this->numeros;
+      public function __construct($_numeros)
+      {
+        $this->numeros = $_numeros;
       }
     }
 
-    // Nome jogador 01
-    $jogador1 = new Dados();
-    $jogador1->set_name($name01);
-    $jogador01 = $jogador1->get_name();
+    $adicionar = array();
 
-    // Números do jogador 01
-    $numeros1 = new Dados();
-    $numeros1->set_numeros(array($jogadores[1], $jogadores[2],$jogadores[3],$jogadores[4],$jogadores[5],$jogadores[6]));
-    $numeros01 = $numeros1->get_numeros();
+    for($i = 0; $i < $tb; $i++)
+    {
+      $adicionar[] = new Dados($teste05[$i]);
+    }
 
-    // // Nome jogador 02
-    // $jogador2 = new Dados();
-    // $jogador2->set_name($name02[1][0]);
-    // $jogador02 = $jogador2->get_name();
+    // print_r($adicionar);
 
-    // // Números do jogador 02
-    // $numeros2 = new Dados();
-    // $numeros2->set_numeros(array($name02[1][1], $name02[1][2],$name02[1][3],$name02[1][4],$name02[1][5],$name02[1][6]));
-    // $numeros02 = $numeros2->get_numeros();
+    // echo "<br><br>";
 
-    // // Nome jogador 03
-    // $jogador3 = new Dados();
-    // $jogador3->set_name($name02[2][0]);
-    // $jogador03 = $jogador3->get_name();
+    // echo $adicionar[1]->name;
 
-    // // Números do jogador 03
-    // $numeros3 = new Dados();
-    // $numeros3->set_numeros(array($name03[2][1], $name03[2][2],$name03[2][3],$name03[2][4],$name03[2][5],$name03[2][6]));
-    // $numeros03 = $numeros3->get_numeros();
+    $adicionar2 = array();
 
-    foreach($numeros01 as $numeros){ 
+    for($i = 0; $i <= 5; $i++)
+    {
+      $adicionar2[] = new Numbers($teste06[$i]);
+    }
+
+    // print_r($adicionar2);
+
+    // echo "<br><br>";
+
+    // echo $adicionar2[1]->numeros;
+
+    $num = 0;
+    for($ty = 0;$ty <= 5; $ty++){
       foreach($array as $certo){      
-        if(intval($numeros) === $certo){
+        if(intval($adicionar2[$ty]->numeros) === $certo){
           $num++;
-          if(3 >= $num){
-            $name = $jogador01;            
+          if(3 <= $num){
+            $name = $adicionar[0]->name;         
+          }else {
+            $name = "";
+            $num = "";
           }
         }
       }    
     }
+    
+
+    // class Dados {
+    //   // Propriedades
+    //   public $name;
+    //   public $numeros;
+
+    //   // Metodos
+    //   function set_name($name) {
+    //     $this->name = $name;
+    //   }
+    //   function get_name() {
+    //     return $this->name;
+    //   }
+
+    //   function set_numeros($numeros) {
+    //     $this->numeros = $numeros;
+    //   }
+    //   function get_numeros() {
+    //     return $this->numeros;
+    //   }
+    // }
+
+    // // Nome jogador 01
+    // $jogador1 = new Dados();
+    // $jogador1->set_name($name01);
+    // $jogador01 = $jogador1->get_name();
+
+    // // Números do jogador 01
+    // $numeros1 = new Dados();
+    // $numeros1->set_numeros(array($jogadores[1], $jogadores[2],$jogadores[3],$jogadores[4],$jogadores[5],$jogadores[6]));
+    // $numeros01 = $numeros1->get_numeros();
+
+    // // Nome jogador 02
+    // $jogador2 = new Dados();
+    // $jogador2->set_name($name02);
+    // $jogador02 = $jogador2->get_name();
+
+    // // Números do jogador 02
+    // $numeros2 = new Dados();
+    // $numeros2->set_numeros(array($jogadores[8], $jogadores[9],$jogadores[10],$jogadores[11],$jogadores[12],$jogadores[13]));
+    // $numeros02 = $numeros2->get_numeros();
+
+    // // Nome jogador 03
+    // $jogador3 = new Dados();
+    // $jogador3->set_name($name02);
+    // $jogador03 = $jogador3->get_name();
+
+    // // Números do jogador 03
+    // $numeros3 = new Dados();
+    // $numeros3->set_numeros(array($jogadores[15], $jogadores[16],$jogadores[17],$jogadores[18],$jogadores[19],$jogadores[20]));
+    // $numeros03 = $numeros3->get_numeros();
+
+    // $array = array(33,44,55,66,77,88);
+
+    // foreach($numeros01 as $numeros){ 
+    //   foreach($array as $certo){      
+    //     if(intval($numeros) === $certo){
+    //       $num++;
+    //       if(3 >= $num){
+    //         $name = $jogador01;            
+    //       }
+    //     }
+    //   }    
+    // }
 
     // foreach($numeros02 as $numeros){ 
     //   foreach($array as $certo){      
@@ -179,11 +298,17 @@
     //     if(intval($numeros) === $certo){
     //       $num2++;
     //       if(3 >= $num2){
-    //         $name2 = $jogador03;            
+    //         $name2 = $jogador03;
     //       }
     //     }
     //   }    
     // } 
   }
+
+  // $total = array($name01 => $num,$name02 => $num1,$name03 => $num2,$name04 => $num3, $name05 => $num4,$name06 => $num5,$name07 => $num6,$name08 => $num7,$name09 => $num8,$name10 => $num9);
+
+  // $max = max($total);
+
+  // echo $max;
 
 ?>
